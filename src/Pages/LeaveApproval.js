d1f717d119436b8281a5629880f67970143173bf
harinim3042 <104339773+harinim3042@users.noreply.github.com>
2022-09-16 15:30:54 +0530

    2 files modified

--- src/Pages/LeaveApproval.js
+++ src/Pages/LeaveApproval.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../style.css';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-import EMP from '../Assets/empData.json';
+// import EMP from '../Assets/empData.json';
 import Table from 'react-bootstrap/Table';
 import Card from 'react-bootstrap/Card';
+import axios from 'axios';
+
+const baseURL = '/Assets/empfullDetails.json';
 
 export default function LeaveApproval() {
+  const [item, setItem] = useState([]);
 
+  React.useEffect(() => {
+    fetch(baseURL)
+      .then((res) => res.json())
+      .then((res) => alert(JSON.stringify(res)))
+      .catch((err) => console.log(err));
+    // axios.get(baseURL).then((response) => {
+    //   alert(JSON.stringify(response));
+    //   setItem(response.data);
+    // });
+  }, []);
+  console.log(Array.isArray(item));
+
+  // if (!item) return null;
   return (
     <div>
       <div>
@@ -33,12 +50,12 @@ export default function LeaveApproval() {
                   <th>REASON</th>
                   <th>APPROVAL STATUS</th>
                 </tr>
-                {EMP.map((item) => (
-                  <tr key={item.id}>
-                    <td>{item.id}</td>
-                    <td>{item.id}</td>
+                {/* {item.map((x) => (
+                  <tr key={x.id}>
+                    <td>{x.id}</td>
+                    <td>{x.id}</td>
                     <td>
-                      {item.first_name} {item.last_name}
+                      {x.first_name} {x.last_name}
                     </td>
                     <td></td>
                     <td></td>
@@ -52,19 +69,19 @@ export default function LeaveApproval() {
                         inline
                         label="APPROVED"
                         type="radio"
-                        id={item.id + "_approved"}
-                        name={item.id}
+                        id={x.id + "_approved"}
+                        name={x.id}
                       />
                       <Form.Check
                         inline
                         label="NOT APPROVED"
                         type="radio"
-                        id={item.id + "_not_approved"}
-                        name={item.id}
+                        id={x.id + "_not_approved"}
+                        name={x.id}
                       />
                     </td>
                   </tr>
-                ))}
+                ))} */}
               </tbody>
             </Table>
             <div className="py-1 pe-5 ps-5 form-center">
