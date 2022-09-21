4f1411e92a87d8774530d29bc0e2031d2baab818
harinim3042 <104339773+harinim3042@users.noreply.github.com>
2022-09-21 15:57:47 +0530

    3 files modified

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,41 +1,39 @@
 import React from 'react';
 import Nav from 'react-bootstrap/Nav';
 
-function AlignmentExample() {
+function NavBar() {
   return (
     <>
-      <div className="text-center mb-5 py-3 nav-bg ">
-        <div className="title">
-        
+      <div className=" mt-n1 py-2  nav-bg ">
+        <div className="title mx-3 pt-2">
           <img
             src=" https://raw.githubusercontent.com/harinim3042/images/main/logo.png"
             height="26px"
           />
           LANTRASOFT
-       
- 
-   
         </div>
-        <Nav className="justify-content-center">
-          <Nav.Item>
-            <Nav.Link href="/">HOME</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link href="/LeaveApplication">LEAVE APPLICATION</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link href="/LeaveApplication">LEAVE STATUS</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link href="/Login">LOGOUT</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link href="/LeaveApproval">LEAVE APPROVAL</Nav.Link>
-          </Nav.Item>
-        </Nav>
-      </div>
+     
+          <Nav className="justify-content-end mt-n5 mb-1 me-2">
+            <Nav.Item>
+              <Nav.Link href="/">HOME</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="/LeaveApplication">LEAVE APPLICATION</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="/LeaveApplication">LEAVE STATUS</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="/LeaveApproval">LEAVE APPROVAL</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="/Login">LOGOUT</Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </div>
+      
     </>
   );
 }
 
-export default AlignmentExample;
+export default NavBar;
