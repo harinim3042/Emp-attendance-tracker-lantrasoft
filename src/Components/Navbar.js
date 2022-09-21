d4803cbff9bf5caec846c929ad19516232f64702
harinim3042 <104339773+harinim3042@users.noreply.github.com>
2022-09-21 13:35:56 +0530

    3 files modified, 1 added

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,44 +1,41 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import Offcanvas from 'react-bootstrap/Offcanvas';
 
-function OffcanvasExample() {
+function AlignmentExample() {
   return (
     <>
-      {[false].map((expand) => (
-        <Navbar key={expand} bg="dark" expand={expand} className="mb-3 ">
-          <Container fluid>
-          <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
-            <Navbar.Brand href="#"><img src=" https://raw.githubusercontent.com/harinim3042/images/main/logo.png" height="26px"/> LANTRASOFT </Navbar.Brand>
-           
-            <Navbar.Offcanvas
-              id={`offcanvasNavbar-expand-${expand}`}
-              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
-              placement="start"
-            >
-              <Offcanvas.Header closeButton>
-                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
-                LANTRASOFT
-                </Offcanvas.Title>
-              </Offcanvas.Header>
-              <Offcanvas.Body>
-                <Nav className="justify-content-end flex-grow-1 pe-3">
-                  <Link to="/">Home</Link>
-                  <Link to="/LeaveApplication">Leave Application</Link>
-                  <Link to="/Login">Logout</Link>
-                  <Link to="/LeaveApproval">Leave</Link>
-                </Nav>
-               
-              </Offcanvas.Body>
-            </Navbar.Offcanvas>
-          </Container>
-        </Navbar>
-      ))}
+      <div className="text-center mb-5 py-3 nav-bg ">
+        <div className="title">
+        
+          <img
+            src=" https://raw.githubusercontent.com/harinim3042/images/main/logo.png"
+            height="26px"
+          />
+          LANTRASOFT
+       
+ 
+   
+        </div>
+        <Nav className="justify-content-center">
+          <Nav.Item>
+            <Nav.Link href="/">HOME</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href="/LeaveApplication">LEAVE APPLICATION</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href="/LeaveApplication">LEAVE STATUS</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href="/Login">LOGOUT</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href="/LeaveApproval">LEAVE APPROVAL</Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </div>
     </>
   );
 }
 
-export default OffcanvasExample;
+export default AlignmentExample;
